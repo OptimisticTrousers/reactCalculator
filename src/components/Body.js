7065410ac2ebec22a82c99ce2d373b81eeb99045
OptimisticTrousers <36343664+OptimisticTrousers@users.noreply.github.com>
2021-10-15 17:22:30 -0400

    moved files over to a seperate components folder

---
+++ src/components/Body.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+
+function button()
+{
+    return("Hello")
+}
+
+export class Body extends Component
+{
+    render(){
+        return(
+            
+        <button onClick={button}>9</button>
+            )
+    }
+
+    
+        
+
+}
+
+export default Body;
